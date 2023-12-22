1771d7adc2a5c534481cf4cc807b5034f2054fd5
Mark Bordakov <mark12bord@gmail.com>
2023-12-22 16:57:51 +0200

    Updated AboutMe page and added new package

--- lib/samples.ts
+++ lib/samples.ts
@@ -1,31 +1,50 @@
-export const certificate = [
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
-    { id: 5 },
-]
-
 export const projects = [
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
-    { id: 5 },
-    { id: 6 },
-    { id: 7 },
-    { id: 8 },
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  { id: 4 },
+  { id: 5 },
+  { id: 6 },
+  { id: 7 },
+  { id: 8 },
 ]
 
 export const articles = [
-    { id: 1, url: '#' },
-    { id: 2, url: '#' },
-    { id: 3, url: '#' },
-    { id: 4, url: '#' },
-    { id: 5, url: '#' },
-    { id: 6, url: '#' },
-    { id: 7, url: '#' },
-    { id: 8, url: '#' },
+  { id: 1, url: '#' },
+  { id: 2, url: '#' },
+  { id: 3, url: '#' },
+  { id: 4, url: '#' },
+  { id: 5, url: '#' },
+  { id: 6, url: '#' },
+  { id: 7, url: '#' },
+  { id: 8, url: '#' },
+]
+
+export const certificate = [
+  { 
+    name: "TypeScript",
+    provider: "ITVDN",
+    date: "",
+    color: "#457fd6",
+  },
+  { 
+    name: "Front-end",
+    provider: "Prometheus",
+    date: "",
+    color: "#6B237D",
+  },
+  { 
+    name: "QA course",
+    provider: "DataArt",
+    date: "",
+    color: "#FF9900",
+  },
+  { 
+    name: "UX/UI design",
+    provider: "ITVDN",
+    date: "",
+    color: "#78A2E1",
+  },
 ]
 
 export const filtersOfProjects = [
@@ -39,6 +58,15 @@ export const filtersOfProjects = [
   },
   {
     key: "type",
+    label: "Scope",
+    options: [
+      { key: "commercial", label: "Commercial" },
+      { key: "pet-project", label: "Pet-project" },
+      // { key: "volunteering", label: "Volunteering" },
+    ],
+  },
+  {
+    key: "type",
     label: "Type",
     options: [
       { key: "calculator", label: "Calculators" },
@@ -46,15 +74,6 @@ export const filtersOfProjects = [
       { key: "generator", label: "Generators" },
       { key: "game", label: "Games" },
       { key: "other", label: "Other" },
-    ],
-  },
-  {
-    key: "type",
-    label: "Scope",
-    options: [
-      { key: "commercial", label: "Commercial" },
-      { key: "pet-project", label: "Pet-project" },
-      // { key: "volunteering", label: "Volunteering" },
     ],
   },
 ]
--- views/AboutMe/index.tsx
+++ views/AboutMe/index.tsx
@@ -1,23 +1,34 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, EffectCoverflow } from 'swiper/modules';
+import { Icon } from '@iconify/react';
 import clsx from 'clsx';
 import { certificate } from '../../lib/samples';
 import Certificate from '../../components/Certificate/index';
+import portrait from '../../public/portrait.jpg';
 import styles from "./styles.module.scss";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/effect-cards';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/effect-cube';
 
 
 const resumes = [
-  {name: 'FE 1'},
-  {name: 'FE 2'},
-  {name: 'GW'},
+  {name: 'FE 1', color: '#672D50', url: 'https://flowcv.com/resume/rrmtgok5b3'},
+  {name: 'GW', color: '#D9D9D9', url: 'https://flowcv.com/resume/vffhu00pnf'},
+  {name: 'FE 2', color: '#502C84', url: 'https://takct-server.netlify.app/CV/M%20Bordakov%20-%20Front-end.pdf'},
 ]
 
 const links = [
-  {name: 'N'},
-  {name: 'N'},
-  {name: 'N'},
-  {name: 'N'},
-  {name: 'N'},
-  {name: 'N'},
+  {name: 'linkedin', icon: 'devicon:linkedin', url: 'https://www.linkedin.com/in/mark-bordakov-8441bb241/'},
+  {name: 'github', icon: 'devicon:github', url: 'https://github.com/mark-bor?tab=repositories'},
+  {name: 'upwork', icon: 'cib:upwork', url: 'https://www.upwork.com/freelancers/~01b8a9c8c7e06fb645'},
+  {name: 'whatsapp', icon: 'logos:whatsapp-icon', url: 'https://wa.me/+380957625669'},
+  {name: 'telegram', icon: 'logos:telegram', url: 'https://t.me/marchi003'},
 ]
 
 export default function AboutMe() {
@@ -25,34 +36,65 @@ export default function AboutMe() {
     <>
       <section className={styles.hero}>
         <div className={styles.leftPartOfBlock}>
-          <h1 className={styles.name}>About Me</h1>
-          <p className={styles.position}>Position</p>
-          <p className={styles.profile}>Profile</p>
+          <h1 className={styles.name}>Mark Bordakov</h1>
+          <p className={styles.position}>Front-end developer</p>
+          <p className={styles.profile}>Dedicated and enthusiastic Junior Front-end Developer with a passion for crafting seamless and user-centric web applications using React and TypeScript. With a solid foundation in web development, I am eager to leverage my skills and creativity to contribute to projects that demand innovative solutions and exceptional user experiences.</p>
         </div>
 
-        <div className={styles.rightPartOfBlock}></div>
+        <div className={styles.rightPartOfBlock}>
+          <Image src={portrait} alt='portrait' className={styles.portrait} />
+        </div>
       </section>
     
       <section className={styles.certificates}>
-        <div className={styles.certificatesWrap}>
-          {certificate.map(c => <Certificate />)}
-        </div>
+        <Swiper
+          className={styles.certificatesWrap}
+          modules={[Navigation, Pagination, EffectCoverflow]}
+          effect='coverflow'
+          // spaceBetween={30}
+          centeredSlides={true}
+          slidesPerView={3}
+          // loop={true}
+          navigation
+          pagination={{clickable: true}}
+          // onSlideChange={() => console.log('slide change')}
+          // onSwiper={(swiper) => console.log(swiper)}
+        >
+          {certificate.map((c) => (
+            <SwiperSlide className={styles.certificate}>
+              <h2 className={styles.certificateName}>{c.name}</h2>
+              <hr className={styles.line} style={{backgroundColor: c.color}} />
+              <p className={styles.provider}>{c.provider}</p>
+              <p className={styles.data}>{c.date}</p>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </section>
     
       <section className={styles.contacts}>
         <div className={clsx(styles.leftPartOfBlock, styles.leftPartOfContacts)}>
-          <p className={styles.phone}>Phone</p>
-          <p className={styles.email}>Email</p>
+          <p className={styles.email}>mark12bord@gmail.com</p>
+          <p className={styles.phone}>+380957625669</p>
 
-          <nav className={styles.resumes}>
-            {resumes.map((r) => 
-              <Link 
-                target={'_blank'} 
-                href={'#'} 
-                className={styles.resume}
-              >{r.name}</Link>
-            )}
-          </nav>
+          <section className={styles.resumes}>
+            <h2 className={styles.resumesTitle}>Resumes:</h2>
+
+            <ul className={styles.resumesList}>
+              {resumes.map((r) => 
+                <li>
+                  <Link 
+                    target={'_blank'} 
+                    href={r.url} 
+                    className={styles.resume}
+                    style={{
+                      backgroundColor: r.color, 
+                      color: r.color==='#D9D9D9' ? '#000000' : '#ffffff'
+                    }}
+                  >{r.name}</Link>
+                </li>
+              )}
+            </ul>
+          </section>
         </div>
 
         <div className={clsx(styles.rightPartOfBlock, styles.rightPartOfContacts)}>
@@ -60,9 +102,16 @@ export default function AboutMe() {
             {links.map((l) => 
               <Link 
                 target={'_blank'} 
-                href={'#'} 
+                href={l.url} 
                 className={styles.link}
-              >{l.name}</Link>
+              >
+                <Icon 
+                  icon={l.icon} 
+                  name={l.name} 
+                  width={50} 
+                  color={l.icon==='cib:upwork' ? '#14A800' : ''} 
+                />
+              </Link>
             )}
           </nav>
         </div>
