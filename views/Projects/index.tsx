44e0db0c3f215afcae7d804784958b8093258989
Mark Bordakov <mark12bord@gmail.com>
2023-12-29 12:55:42 +0200

    Revert "Optimized About Me page for mobile"
    This reverts commit 45db2021649631428daf9abed36029fe61b1374a.

--- views/Projects/index.tsx
+++ views/Projects/index.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { Icon } from '@iconify/react';
-import clsx from 'clsx';
+import React from 'react';
+import Link from 'next/link';
 import Page from '../../components/Page/index';
 import { projects, filtersOfProjects } from '../../lib/samples';
 import ProjectFilters from '../../components/ProjectFilters/index';
@@ -8,20 +7,10 @@ import styles from "./styles.module.scss";
 
 
 export default function Projects() {
-  const [filters, setFilters] = useState(false);
-
   return (
     <Page title='The Kitsune Diary | Projects'>
       <section className={styles.projects}>
-        <button className={styles.filterButtonOpen} onClick={() => setFilters(!filters)}>
-          <Icon icon={'ion:filter'} width={30} />
-        </button>
-
-        <section className={clsx(styles.filters, filters ? styles.filtersActive : null)}>
-          <button className={styles.filterButtonClose} onClick={() => setFilters(!filters)}>
-            <Icon icon={'radix-icons:cross-2'} width={30} />
-          </button>
-
+        <section className={styles.filters}>
           <h2 className={styles.title}>Filter by</h2>
 
           <ProjectFilters filters={filtersOfProjects} />
@@ -29,7 +18,9 @@ export default function Projects() {
 
         <ul className={styles.projectsList}>
           {projects.map((p) => (
-            <li className={styles.project}></li>
+            <li className={styles.project}>
+
+            </li>
           ))}
         </ul>
       </section>
