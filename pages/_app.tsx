5a231c8f470be2ecadc6a1c83ee258a17e8dab5b
Mark Bordakov <mark12bord@gmail.com>
2024-01-02 14:23:33 +0200

    Added font to whole app and to Hero on Home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
+import { Inter } from 'next/font/google'
 import type { AppProps } from 'next/app';
 import "../styles/global.scss";
 
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['sans-serif'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
  
-export default function App({ Component, pageProps }: AppProps) {
+export default function TheKitsuneDiary({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className={inter.className}>
       <Component {...pageProps} />
-    </>
+    </div>
   )
 }
 
