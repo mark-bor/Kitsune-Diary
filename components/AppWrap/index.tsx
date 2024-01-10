2d88f204eadb8c6300d36ac7186702472bfeec52
Mark Bordakov <mark12bord@gmail.com>
2024-01-10 16:57:31 +0200

    Added parallax effect to website background

--- components/AppWrap/index.tsx
+++ components/AppWrap/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import { useAppSelector } from '../../lib/redux/hook'
 import { Inter } from 'next/font/google'
 import clsx from 'clsx'
@@ -17,9 +18,28 @@ type AppThemeData = {
 
 export default function AppWrap({children}: AppThemeData) {
     const theme = useAppSelector(state => state.theme);
+    const [position, setPosition] = useState(0);
+
+    const scroll = () => {
+        const appWraper = document.getElementById('app-wraper');
+        const imageLeft = Number(appWraper?.clientWidth) - window.innerHeight;
+        const y: number = 100 * window.pageYOffset / imageLeft;
+        setPosition(y);
+    }
+
+    useEffect(() => {
+        if (window.innerWidth > 425) {
+            window.addEventListener('scroll', scroll);
+        }
+    })
 
     return (
-        <div className={clsx(inter.className, styles.appWrap)} data-theme={theme}>
+        <div 
+            id='app-wraper'
+            className={clsx(inter.className, styles.appWrap)} 
+            data-theme={theme}
+            style={{backgroundPositionY: `-${position}px`}}
+        >
             {children}
         </div>
     )
